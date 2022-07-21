7aa8f48f9bd72d26ab8a09392a85490c9c1935ca
오병욱 <obw@192.168.nate.com>
2022-07-21 22:12:17 +0900

    day4

---
+++ 0721_12.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int people;
+    int total, p_w;
+    int c_num;
+
+    printf("Caribbean Bay Rate Calculation\n\n");
+    printf("Select an option\n");
+    printf("1.Smart Booking\n");
+    printf("2.KB Kukmin Card and Hana Card\n");
+    printf("3.Middle and High school Student");
+
+    printf("Choice Number(1-3): ");
+    scanf("%d", &c_num);
+    switch(c_num)
+    {
+        case 1:
+            p_w = 29900;
+            break;
+        case 2:
+            p_w = 37000;
+            break;
+        case 3:
+            p_w = 21000;
+    }
+        printf("people number : ");
+        scanf("%d", &people);
+
+        total = p_w * people;
+
+        printf("\n\n==========Rate Calculation==========\n");
+        printf("people number : %d\n", people);
+        printf("total : %d", total);
+}
+
